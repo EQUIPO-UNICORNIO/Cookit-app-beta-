c6efad445b5802beb00450ba393ff789dcb255d3
Junkai <junkail.jl@gmail.com>
2026-05-17 20:24:00 +0200

    chore: clean repo - remove node_modules, dist, logs, bat, txt files

--- tunnel.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\arone\Downloads\stitch_cookit_smart_meal_planner"
-ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -R 80:127.0.0.1:3001 nokey@localhost.run
-pause
