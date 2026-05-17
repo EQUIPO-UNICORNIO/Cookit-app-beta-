0a8363be80873ddbd7e7893f74803a96cf652c0f
Junkai <junkail.jl@gmail.com>
2026-05-17 18:18:39 +0200

    fix vercel outputDirectory config

--- api/index.js
+++ api/index.js
@@ -1,14 +1,3 @@
-const express = require('express');
-const path = require('path');
 const app = require('../backend/app');
-
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
-app.get('*', (req, res) => {
-  if (!req.path.startsWith('/api')) {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
-  } else {
-    res.status(404).json({ error: 'Ruta no encontrada' });
-  }
-});
 
 module.exports = app;
